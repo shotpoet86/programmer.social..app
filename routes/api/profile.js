7bc9965135434238e03d94d3786d4a8c94d1da5c
shotpoet86 <curlybracemaster@gmail.com>
2020-12-22 16:36:26 -0500

    -completed backend authentication and authorization on auth.js, users.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -87,4 +87,30 @@ router.post('/', [auth, [check('status', 'Status is required').not().isEmpty(),
         }
     });
 
+/* @route   GET api/profile
+*  @desc    Get all profiles
+*  @access  Public*/
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+/* @route   GET api/profile/user/user_id
+*  @desc    Get profile by userID
+*  @access  Public*/
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar']);
+        if (!profile) return res.status(400).json({msg: 'There is no profile for this user'});
+        res.json(profiles);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
